ad498f6cfd25803ed1fbbf3fc6e231aa89859bbe
timger <yishenggudou@gmail.com>
2019-01-10 22:45:59 +0800

    BugFix: 1.
    NewAdd:
    1.

--- build.doc.sh
+++ build.doc.sh
@@ -5,7 +5,7 @@ rm -vrf ./dist
 mkdir -p dist
 cp -vrf ./landing-page/* ./dist
 cd ./docs/blog/
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 make clean html
 cd -
 mkdir -p ./dist/docs
