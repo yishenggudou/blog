75b1583afb76e880ec5afba8beb7eca1125184ec
timger <yishenggudou@gmail.com>
2019-01-14 11:36:27 +0800

    BugFix: 1.
    NewAdd:
    1.

--- build.doc.sh
+++ build.doc.sh
@@ -3,12 +3,12 @@ cd $(dirname $0)
 DIR=`pwd`
 rm -vrf ./dist
 mkdir -p dist
-cp -vrf ./landing-page/* ./dist
+cp -rf ./landing-page/* ./dist
 cd ./docs/blog/
 pip install -r requirements.txt
 make clean html
 cd -
 mkdir -p ./dist/docs
-cp  -vrf ./docs/blog/_build/html ./dist/docs/blog
+cp  -rf ./docs/blog/_build/html ./dist/docs/blog
 mkdir -p /data/sites/timger/
-cp -vrf ./dist/* /www/sites/timger
+cp -rf ./dist/* /www/sites/timger
