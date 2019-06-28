1543cc30937d3a7348d4a31d337d6b77a0aadf64
timger <yishenggudou@gmail.com>
2019-06-29 00:19:54 +0800

    add

--- build.doc.sh
+++ build.doc.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 DIR=`pwd`
-pip install -r requirements.txt
 rm -vrf ./dist
 mkdir -p dist
 cp -rf ./landing-page/* ./dist
 cd ./docs/blog/
-pip install -r requirements.txt
 make clean html
 rc=$?; if [[ ${rc} != 0 ]]; then exit 1 ${rc}; fi
 cd -
 mkdir -p ./dist/docs
 cp  -rf ./docs/blog/_build/html ./dist/docs/blog
-mkdir -p /data/sites/timger/
-cp -rf ./dist/* /www/sites/timger
+cd ./dist
